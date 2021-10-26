c9d315a3e0deeb9d7cebe3e73d9cd542a0a211ab
CennoxX <cesar.bernard@gmx.de>
2021-10-26 16:59:06 +0200

    correct supportURL

--- Get label from Fernsehserien.de.user.js
+++ Get label from Fernsehserien.de.user.js
@@ -8,7 +8,7 @@
 // @homepage     https://github.com/CennoxX/userscripts/
 // @downloadURL  https://greasyfork.org/scripts/429117-get-label-from-fernsehserien-de/code/Get%20label%20from%20Fernsehseriende.user.js
 // @updateURL    https://greasyfork.org/scripts/429117-get-label-from-fernsehserien-de/code/Get%20label%20from%20Fernsehseriende.meta.js
-// @supportURL   https://github.com/CennoxX/userscripts/issues/new?title=[Get%20label%20from%20Fernsehserien.de.user.js]%20
+// @supportURL   https://github.com/CennoxX/userscripts/issues/new?title=[Get%20label%20from%20Fernsehserien.de]%20
 // @match        https://www.wikidata.org/wiki/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikidata.org
 // @grant        GM.xmlHttpRequest
