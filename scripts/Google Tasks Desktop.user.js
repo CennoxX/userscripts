c599eef0f38be19e962b409520ecbc6a251186c7
CennoxX <cesar.bernard@gmx.de>
2021-11-30 11:37:31 +0100

    update icon
    fixes #4

--- scripts/Google Tasks Desktop.user.js
+++ scripts/Google Tasks Desktop.user.js
@@ -10,7 +10,7 @@
 // @updateURL    https://greasyfork.org/scripts/429123-google-tasks-desktop/code/Google%20Tasks%20Desktop.meta.js
 // @supportURL   https://github.com/CennoxX/userscripts/issues/new?title=[Google%20Tasks%20Desktop]%20
 // @match        https://fullscreen-for-googletasks.com/
-// @icon         https://ssl.gstatic.com//tasks/00d84c8baaaf6dd434993369f1441e47/favicon.ico
+// @icon         https://ssl.gstatic.com//tasks/f5cfbf604fef12b05341c945d745fff2/favicon.ico
 // @grant        GM.addStyle
 // @license      MIT
 // ==/UserScript==
@@ -18,7 +18,7 @@
 (function() {
     'use strict';
     var favicon = document.querySelector("link[rel~='icon']");
-    favicon.href = 'https://ssl.gstatic.com//tasks/00d84c8baaaf6dd434993369f1441e47/favicon.ico';
+    favicon.href = 'https://ssl.gstatic.com//tasks/f5cfbf604fef12b05341c945d745fff2/favicon.ico';
     GM.addStyle(".sc-fzqLLg,.sc-fznXWL,.hejSGq input,.eQYbes .MuiPaper-root{background-color:White}");
     GM.addStyle(".hejSGq input,.eQYbes .MuiPaper-root{border:1px solid #b1b1b1}");
     setTimeout(()=>{document.querySelector(".sc-fzqLLg>span").innerText = "Google Tasks"},1000);
