2d64ea7711c6c4920d2b8a52c8d0f9ce5a261636
CennoxX <cesar.bernard@gmx.de>
2024-11-05 12:13:49 +0100

    use SPARQL request instead of multiple API Requests
    use paging to fix counting more than 50 episodes
    fix space on "Stimme von"
    hide unreleased works

--- scripts/Filmografie von IMDb nach Wikipedia.user.js
+++ scripts/Filmografie von IMDb nach Wikipedia.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Filmografie von IMDb nach Wikipedia
-// @version      3.0.1
+// @version      4.0.0
 // @description  Wandelt die Filmografie von IMDb mithilfe von Wikidata in Wikipedia-Quelltext um
 // @author       CennoxX
 // @namespace    https://greasyfork.org/users/21515
@@ -32,22 +32,14 @@
             var isMale = document.querySelector(".filmo-section-actor,.filmo-section-actress").classList.contains("filmo-section-actor");
             var imdbId = location.href.split("/")[4];
             var filmography = [];
-            var request = 0;
             var done = 0;
-            var resp = await fetch(`https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables={"after":"${btoa("nm0000001/tt0000001/actor")}","id":"${imdbId}","includeUserRating":false,"locale":"en-US"}&extensions={"persistedQuery":{"sha256Hash":"${(isMale ? sha256.loadActor : sha256.loadActress)}","version":1}}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "x-imdb-user-country": "DE"
-                }
-            });
-            var workObj = await resp.json();
-            var work = workObj.data.name[(isMale ? "actor_credits" : "actress_credits")].edges.map(i => i.node);
-            var checkIfCompleted = setInterval(() => {
-                console.clear();
-                if ((done / request) != 1){
+            var request = 1;
+            var checkIfCompleted = setInterval(async () => {
+                if (done / request != 1){
+                    console.clear();
                     console.log("requests:", done, "/", request);
                 } else {
+                    await getDataFromWikidata();
                     var formattedFilmography = "== Filmografie ==";
                     filmography.forEach(entry => {
                         formattedFilmography += entry.toString();
@@ -57,16 +49,29 @@
                     if (showAlert){
                         alert(successMessage);
                     } else {
+                        console.clear();
                         console.log(formattedFilmography);
                         console.log(successMessage);
                     }
                     clearInterval(checkIfCompleted);
                 }
             }, 1000);
-
+            var resp = await fetch(`https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables={"id":"${imdbId}","includeUserRating":false,"locale":"de-DE"}&extensions={"persistedQuery":{"sha256Hash":"${(isMale ? sha256.loadActor : sha256.loadActress)}","version":1}}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "x-imdb-user-country": "DE"
+                }
+            });
+            var workObj = await resp.json();
+            var work = workObj.data.name[(isMale ? "actor_credits" : "actress_credits")].edges.map(i => i.node);
+            done++;
             for(var w of work){
                 var credit = new Credit();
-                credit.yearFrom = w.episodeCredits.yearRange?.year ?? w.title.releaseYear.year;
+                credit.yearFrom = w.episodeCredits.yearRange?.year ?? w.title.releaseYear?.year;
+                if (!credit.yearFrom){
+                    continue;
+                }
                 credit.yearTo = w.episodeCredits.yearRange ? w.episodeCredits.yearRange.endYear ?? 0 : 0;
                 credit.dt = w.title.titleText.text.replace(" - ", " – ").replace("...", "…");
                 credit.ot = w.title.originalTitleText.text.replace(" - ", " – ").replace("...", "…");
@@ -90,32 +95,8 @@
                 credit.voice = w.attributes?.[0].text == "voice";
                 credit.role = w.characters?.[0].name;
                 filmography.push(credit);
-                getItemFromWikidata(credit.imdbid);
                 if (credit.type.includes("serie")){
-                    credit.numberOfEpisodes = w.episodeCredits.total;
-                    request++;
-                    resp = await fetch(`https://caching.graphql.imdb.com/?operationName=EpisodeBottomSheetCast&variables={"after":"","episodeCreditsFilter":{},"locale":"de-DE","nameId":"${imdbId}","titleId":"${credit.imdbid}"}&extensions={"persistedQuery":{"sha256Hash":"${(isMale ? sha256.loadEpisodeActor : sha256.loadEpisodeActress)}","version":1}}`, {
-                        method: "GET",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "x-imdb-user-country": "DE"
-                        }
-                    });
-                    var epData = await resp.json();
-                    done++;
-                    var ep = epData.data.name.credits.edges[0]?.node;
-                    if (ep){
-                        credit.numberOfEpisodes = ep.episodeCredits.edges.filter(i => i.node.attributes?.[0]?.text != "credit only").length;
-                        if (credit.numberOfEpisodes == 1){
-                            var epCredit = ep.episodeCredits.edges[0].node.title;
-                            credit.episodeid = epCredit.id;
-                            if (epCredit.series.displayableEpisodeNumber.episodeNumber.text != "unknown")
-                                credit.episodeName = (epCredit.series.displayableEpisodeNumber.displayableSeason.text + "x" + epCredit.series.displayableEpisodeNumber.episodeNumber.text).replace(/x(\d)$/,"x0$1");
-                            getItemFromWikidata(credit.episodeid);
-                        } else {
-                            credit.episodeName = "";
-                        }
-                    }
+                    getDataFromIMDb(credit.imdbid, "");
                 }
             }
 
@@ -123,78 +104,98 @@
                 return a.yearFrom - b.yearFrom;
             });
 
-            function getItemFromWikidata(imdbid){
+            function getDataFromIMDb(titleId, page){
                 request++;
                 GM.xmlHttpRequest({
                     method: "GET",
-                    url: "https://www.wikidata.org/w/api.php?action=query&format=json&list=search&srsearch=haswbstatement:P345=" + imdbid,
+                    url: `https://caching.graphql.imdb.com/?operationName=EpisodeBottomSheetCast&variables={"after":"${page}","episodeCreditsFilter":{},"locale":"de-DE","nameId":"${imdbId}","titleId":"${titleId}"}&extensions={"persistedQuery":{"sha256Hash":"${(isMale ? sha256.loadEpisodeActor : sha256.loadEpisodeActress)}","version":1}}`,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "x-imdb-user-country": "DE"
+                    },
                     onload: function(response){
                         done++;
-                        if (response.responseText.length > 0){
-                            var jsonObj = JSON.parse(response.responseText);
+                        var respText = response.responseText;
+                        if (respText.length > 0){
+                            var jsonObj = JSON.parse(respText);
                             var credit = filmography.find(c => {
-                                return c.imdbid == imdbid || c.episodeid == imdbid;
+                                return c.imdbid == titleId;
                             });
-                            if (jsonObj.query.searchinfo.totalhits > 0){
-                                var wikidataid = jsonObj.query.search[0].title;
-                                if (credit.imdbid == imdbid){
-                                    credit.link = wikidataid;
-                                } else {
-                                    credit.episodeid = wikidataid;
+                            var ep = jsonObj.data.name.credits.edges[0]?.node;
+                            if (credit && ep){
+                                credit.numberOfEpisodes += ep.episodeCredits.edges.filter(i => i.node.attributes?.[0]?.text != "credit only").length;
+                                if (ep.episodeCredits.pageInfo.hasNextPage){
+                                    getDataFromIMDb(titleId, ep.episodeCredits.pageInfo.endCursor);
                                 }
-                                getDataFromWikidata(wikidataid);
+                                if (credit.numberOfEpisodes == 1){
+                                    var epCredit = ep.episodeCredits.edges[0].node.title;
+                                    credit.episodeid = epCredit.id;
+                                    if (epCredit.series.displayableEpisodeNumber.episodeNumber.text != "unknown"){
+                                        credit.episodeName = (epCredit.series.displayableEpisodeNumber.displayableSeason.text + "x" + epCredit.series.displayableEpisodeNumber.episodeNumber.text).replace(/x(\d)$/,"x0$1");
+                                    }
+                                } else {
+                                    credit.episodeName = "";
+                                }
                             }
                         }
                     },
                     onerror: function(response){
                         done++;
-                        console.log("Error in fetching contents: " + response.responseText);
+                        console.error("Error in fetching contents: " + response.responseText);
                     }
                 });
             }
 
-            function getDataFromWikidata(wikidataid){
+            async function getDataFromWikidata(){
                 request++;
-                GM.xmlHttpRequest({
-                    method: "GET",
-                    url: "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&props=sitelinks|claims|labels&sitefilter=dewiki&ids=" + wikidataid,
-                    onload: function(response){
-                        done++;
-                        if (response.responseText.length > 0){
-                            var jsonObj = Object.values(JSON.parse(response.responseText).entities)[0];
-                            var credit = filmography.find(c => {
-                                return c.link == wikidataid || c.episodeid == wikidataid;
-                            });
-                            if (credit.link == wikidataid){ //get dt, ot, link
-                                if (typeof jsonObj.sitelinks.dewiki != "undefined"){ //wikipedia article
-                                    credit.link = jsonObj.sitelinks.dewiki.title;
-                                } else {
-                                    credit.link = "";
-                                }
-                                if (typeof jsonObj.labels.de != "undefined"){ //wikidata label
-                                    credit.dt = jsonObj.labels.de.value;
-                                }
-                                if (typeof jsonObj.claims.P1476 != "undefined"){ //check if OT of entity exists
-                                    credit.ot = jsonObj.claims.P1476[0].mainsnak.datavalue.value.text.replace(/'/g, "’");
-                                }
-                            } else if (credit.episodeid == wikidataid){ //get episode name
-                                if (typeof jsonObj.sitelinks.dewiki != "undefined"){ //wikipedia article
-                                    var article = jsonObj.sitelinks.dewiki.title;
-                                    if (article.slice(-1) == ")"){
-                                        article += `|${article.split(" (")[0]}`;
-                                    }
-                                    credit.episodeName += ` ''[[${article}]]''`;
-                                } else if (typeof jsonObj.labels.de != "undefined"){ //wikidata label
-                                    credit.episodeName += ` ''${jsonObj.labels.de.value}''`;
-                                }
-                            }
-                        }
+                var imdbIds = filmography.flatMap(i => [i.episodeid, i.imdbid]).filter(i => i).join('" "');
+                var sparqlQuery = `SELECT ?imdb ?dt (SAMPLE(?ot) AS ?ot) ?dewiki WHERE {
+                VALUES ?imdb { "${imdbIds}" }
+                ?item wdt:P345 ?imdb.
+                OPTIONAL {
+                    ?item rdfs:label ?dt.
+                    FILTER((LANG(?dt)) = "de")
+                }
+                OPTIONAL {
+                    ?item wdt:P1476 ?ot.
+                }
+                OPTIONAL {
+                    ?dewiki schema:about ?item;
+                            schema:isPartOf <https://de.wikipedia.org/>.
+                }
+            }
+            GROUP BY ?imdb ?dt ?dewiki`;
+                var resp = await fetch("https://query.wikidata.org/sparql?format=json", {
+                    "headers": {
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
                     },
-                    onerror: function(response){
-                        done++;
-                        console.log("Error in fetching contents: " + response.responseText);
+                    "body": "query=" + encodeURIComponent(sparqlQuery),
+                    "method": "POST",
+                    "mode": "cors"
+                });
+                var jsonObj = await resp.json();
+                jsonObj.results.bindings.forEach(i => {
+                    var credit = filmography.find(c => c.imdbid == i.imdb.value);
+                    i.dewiki = i.dewiki ? decodeURIComponent(i.dewiki.value).slice(30).replace(/_/g," ") : "";
+                    if (credit){
+                        credit.link = i.dewiki;
+                        credit.dt = i.dt?.value || credit.dt;
+                        credit.ot = i.ot?.value.replace(/'/g, '’') || credit.ot;
+                    }
+                    var episodeCredit = filmography.find(c => c.episodeid == i.imdb.value);
+                    if (episodeCredit){
+                        if (i.dewiki){
+                            var article = i.dewiki;
+                            if (article.slice(-1) == ")"){
+                                article += `|${article.split(" (")[0]}`;
+                            }
+                            episodeCredit.episodeName += ` ''[[${article}]]''`;
+                        } else if (i.dt){
+                            episodeCredit.episodeName += ` ''${i.dt?.value}''`;
+                        }
                     }
                 });
+                done++;
             }
 
             function Credit(){
@@ -252,7 +253,7 @@
                     }
                     if (this.voice){
                         if (this.role){
-                            descriptionPart += (descriptionPart ? ", " : " (") + "Stimme von ''" + this.role + "''";
+                            descriptionPart += (descriptionPart ? ", " : " (") + "Stimme von ''" + this.role.split(" (")[0] + "''";
                         }else{
                             descriptionPart += (descriptionPart ? ", " : " (") + "Sprechrolle";
                         }
@@ -271,8 +272,8 @@
     GM.registerMenuCommand("Filmografie laden",() => {
         unsafeWindow.ladeFilmografie(undefined, undefined, true);
     },"f");
+
     console.log('Um die Filmografie mit Standardeinstellungen zu laden, genügt ein Klick im Menü des Userscripts auf "Filmografie laden".\n' +
-                'Die Filmografie kann in der Console mit erweiterten Einstellungen in der Form "ladeFilmografie(occupation, showShort, episodeLabel);" aufgerufen werden.\n' +
-                'Dabei steht "episodeLabel" für die verwendete Bezeichnung "Folge" oder "Episode" und "showShort" dafür, ob Kurzfilme aufgeführt werden sollen oder nicht (true oder false).\n' +
-                'Mit "occupation" können andere Filmografien als Schauspiel-Filmografien ausgegeben werden, dazu ist zum Beispiel "writer", "director" oder "producer" anzugeben.');
+                'Die Filmografie kann in der Console mit erweiterten Einstellungen in der Form "ladeFilmografie(showShort, episodeLabel);" aufgerufen werden.\n' +
+                'Dabei steht "episodeLabel" für die verwendete Bezeichnung "Folge" oder "Episode" und "showShort" dafür, ob Kurzfilme aufgeführt werden sollen oder nicht (true oder false).');
 })();
